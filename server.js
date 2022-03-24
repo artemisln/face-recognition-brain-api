d8f5374229635efbc1bb511167ea32cb4517f90e
artemisln <78040142+artemisln@users.noreply.github.com>
2022-03-24 22:01:52 +0200

    sk

--- server.js
+++ server.js
@@ -12,11 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-fitted-28432',
-    user : 'artem',
-    port: 5432,
-    password : '',
-    database : 'face-recognition-brain'
+    host : process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
